fdf46123a4756105397088a0e57058ccaa4d8432
Emmalex22 <emmanuelpelumi22@gmail.com>
2023-09-16 15:25:37 -0700

    updated

--- main.h
+++ main.h
@@ -8,10 +8,7 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 void print_number(int n);
-<<<<<<< HEAD
 int _strlen(const char *str);
-=======
 void _printf_int(char *format, ...);
->>>>>>> 8f1e0d246ff724d05d2487497a45d1c0727e51b4
 
 #endif
